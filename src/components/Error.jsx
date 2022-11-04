56fe8ec97318cef3164d73853c6294e1681f23f6
krish-98 <muralikrishnanm98@gmail.com>
2022-11-04 13:46:53 +0530

    added error component if there is any error

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React from "react"
 
 const Error = () => (
-  <div>Error</div>
-);
+  <div className="w-fu11 flex justify-center items-center">
+    <hl className="font-bold text-2x1 text-white mt-2">
+      Something went wrong. Please try again.
+    </hl>
+  </div>
+)
 
-export default Error;
+export default Error
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -30,7 +30,7 @@ const Discover = () => {
         </select>
       </div>
 
-      <div className="flex flex-wrap sm:justify-start justify-center gap-8 text-white">
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8 ">
         {data?.map((song, i) => (
           <SongCard key={song.key} song={song} i={i} />
         ))}
