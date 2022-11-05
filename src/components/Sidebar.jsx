f2fe7c0f4bf98887f1e001c6c0c54bb518da2148
krish-98 <muralikrishnanm98@gmail.com>
2022-11-05 23:06:01 +0530

    adding sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,34 @@
-const Sidebar = () => <div>Sidebar</div>
+import { useState } from "react"
+import { NavLink } from "react-router-dom"
+import { RiCloseLine } from "react-icons/ri"
+
+import { logo } from "../assets"
+import { links } from "../assets/constants"
+
+const NavLinks = ({ handleClick }) => (
+  <div>
+    {links.map((item) => (
+      <NavLink
+        key={item.name}
+        to={item.to}
+        onClick={() => handleClick && handleClick()}
+        className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400"
+      >
+        <item.icon className="w-6 h-6 mr-2" />
+        {item.name}
+      </NavLink>
+    ))}
+  </div>
+)
+
+const Sidebar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  return (
+    <div className="hidden md:flex flex-col w-[240px] py-10 px-4 bg-[#191624]">
+      <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+      <NavLinks />
+    </div>
+  )
+}
 
 export default Sidebar
