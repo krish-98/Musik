ed90334973a47d35eb2d1b7f7764d397276eefd3
krish-98 <muralikrishnanm98@gmail.com>
2022-11-06 13:24:48 +0530

    mobile toggling sidebar + desktop navbar added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { NavLink } from "react-router-dom"
 import { RiCloseLine } from "react-icons/ri"
+import { HiOutlineMenu } from "react-icons/hi"
 
 import { logo } from "../assets"
 import { links } from "../assets/constants"
@@ -24,10 +25,35 @@ const NavLinks = ({ handleClick }) => (
 const Sidebar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
   return (
-    <div className="hidden md:flex flex-col w-[240px] py-10 px-4 bg-[#191624]">
-      <img src={logo} alt="logo" className="w-full h-14 object-contain" />
-      <NavLinks />
-    </div>
+    <>
+      <div className="hidden md:flex flex-col w-[240px] py-10 px-4 bg-[#191624]">
+        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <NavLinks />
+      </div>
+
+      {/* Mobile Navbar */}
+      <div className="absolute md:hidden block top-6 right-3">
+        {mobileMenuOpen ? (
+          <RiCloseLine
+            onClick={() => setMobileMenuOpen(false)}
+            className="w-6 h-6 text-white mr-2 cursor-pointer"
+          />
+        ) : (
+          <HiOutlineMenu
+            onClick={() => setMobileMenuOpen(true)}
+            className="w-6 h-6 text-white mr-2 cursor-pointer"
+          />
+        )}
+      </div>
+      <div
+        className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483d8b] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${
+          mobileMenuOpen ? "left-0" : "-left-full"
+        }`}
+      >
+        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <NavLinks handleClick={() => setMobileMenuOpen(false)} />
+      </div>
+    </>
   )
 }
 
