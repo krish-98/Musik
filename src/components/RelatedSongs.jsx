e8b072cc256e234ed81ad478ea9e18ab93a34ebf
krish-98 <muralikrishnanm98@gmail.com>
2022-11-08 20:01:45 +0530

    showing related songs

--- src/components/RelatedSongs.jsx
+++ src/components/RelatedSongs.jsx
@@ -1,5 +1,28 @@
-const RelatedSongs = () => (
-  <div>Loader</div>
-);
+import SongBar from "./SongBar"
 
-export default RelatedSongs;
+const RelatedSongs = ({
+  data,
+  artistId,
+  isPlaying,
+  activeSong,
+  handlePauseClick,
+  handlePlayClick,
+}) => (
+  <div className="flex flex-col">
+    <h1 className="font-bold text-3xl text-white">Related Songs:</h1>
+    {data?.map((song, i) => (
+      <SongBar
+        key={`${song.key}-${artistId}`}
+        song={song}
+        i={i}
+        artistId={artistId}
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+        handlePauseClick={handlePauseClick}
+        handlePlayClick={handlePlayClick}
+      />
+    ))}
+  </div>
+)
+
+export default RelatedSongs
--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -3,7 +3,10 @@ import { useSelector, useDispatch } from "react-redux"
 import { DetailsHeader, Error, Loader, RelatedSongs } from "../components"
 
 import { setActiveSong, playPause } from "../redux/features/playerSlice"
-import { useGetSongDetailsQuery } from "../redux/services/shazamCore"
+import {
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
+} from "../redux/services/shazamCore"
 
 const SongDetails = () => {
   const dispatch = useDispatch()
@@ -11,9 +14,28 @@ const SongDetails = () => {
   const { activeSong, isPlaying } = useSelector((state) => state.player)
   const { data: songData, isFetching: isFetchingSongDetails } =
     useGetSongDetailsQuery({ songid })
+  const {
+    data,
+    isFetching: isFetchingRelatedSongs,
+    error,
+  } = useGetSongRelatedQuery({ songid })
+
+  const handlePlayClick = (song, i) => {
+    dispatch(setActiveSong({ song, data, i }))
+    dispatch(playPause(true))
+  }
+
+  const handlePauseClick = () => {
+    dispatch(playPause(false))
+  }
 
   console.log(songid)
   console.log(songData)
+
+  if (isFetchingSongDetails || isFetchingRelatedSongs)
+    return <Loader title="Searching song details" />
+
+  if (error) return <Error />
 
   return (
     <div className="flex flex-col">
@@ -35,6 +57,14 @@ const SongDetails = () => {
           )}
         </div>
       </div>
+
+      <RelatedSongs
+        data={data}
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+        handlePauseClick={handlePauseClick}
+        handlePlayClick={handlePlayClick}
+      />
     </div>
   )
 }
