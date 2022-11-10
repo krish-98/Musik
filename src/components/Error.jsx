ba5c228d9d6693a70c9122fe3e7c52401cc85abb
krish-98 <muralikrishnanm98@gmail.com>
2022-11-10 15:23:33 +0530

    tag name misspelled

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -2,9 +2,9 @@ import React from "react"
 
 const Error = () => (
   <div className="w-fu11 flex justify-center items-center">
-    <hl className="font-bold text-2x1 text-white mt-2">
+    <h1 className="font-bold text-2x1 text-white mt-2 ">
       Something went wrong. Please try again.
-    </hl>
+    </h1>
   </div>
 )
 
